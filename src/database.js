3d89efb90725824e831f9ae2761c3b649a5559cd
cdgalvis <cristiangalviscardona@gmail.com>
2024-05-28 20:49:05 +0200

    Proyecto con bd vercel

--- src/database.js
+++ src/database.js
@@ -2,12 +2,16 @@ import pg  from 'pg'
 const { Pool } = pg
 
 const dbConfig = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+})
+
+/*const dbConfig = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'chatapp',
     password: 'Admin12345',
     port: 5432
-});
+});*/
 
 
 export default dbConfig;
