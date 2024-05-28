fa1f41a7b197b99179b54b3e3a9c93e630c68e3a
cdgalvis <cristiangalviscardona@gmail.com>
2024-05-28 18:55:34 +0200

    Primer carga 3

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import express from 'express'
-import morgan from 'morgan';
 
 const app = express()
-app.use(morgan('dev'))
-app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.get('/', (req, res)=>{
